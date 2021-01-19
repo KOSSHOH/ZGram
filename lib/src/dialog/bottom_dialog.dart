f9deeaefc36f327bf04cfd71e33b8cd7050e12e0
Shahboz <shahboz.turonov98@gmail.com>
2021-01-19 18:46:57 +0500

    comment added dialog

--- lib/src/dialog/bottom_dialog.dart
+++ lib/src/dialog/bottom_dialog.dart
@@ -517,7 +517,7 @@ class BottomDialog {
                               GestureDetector(
                                 onTap: () {
                                   var now = new DateTime.now();
-
+                                  FocusScopeNode currentFocus = FocusScope.of(context);
                                   if (isSend) {
                                     Utils.getName().then(
                                       (value) => {
@@ -528,9 +528,15 @@ class BottomDialog {
                                           tapeId: tapeId,
                                         )),
                                         homeBloc.fetchUpdateComment(),
+                                        BottomDialog.sendCommentSuccess(
+                                            context),
                                         setState(() {
                                           chatController.text = "";
                                         }),
+                                        if (!currentFocus.hasPrimaryFocus)
+                                          {
+                                            currentFocus.unfocus(),
+                                          },
                                       },
                                     );
                                   }
@@ -547,6 +553,113 @@ class BottomDialog {
                     ],
                   ),
                 ),
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+
+  static void sendCommentSuccess(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      builder: (BuildContext context) {
+        return StatefulBuilder(
+          builder: (BuildContext context, setState) {
+            return Container(
+              height: 487,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(10),
+                  topRight: Radius.circular(10),
+                ),
+                color: AppTheme.white,
+              ),
+              child: Column(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(
+                      top: 15,
+                    ),
+                    height: 5,
+                    width: 114,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(
+                        3,
+                      ),
+                      color: AppTheme.grey20,
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(
+                      top: 50,
+                    ),
+                    height: 78,
+                    width: 78,
+                    child: SvgPicture.asset(
+                      "assets/icon/success.svg",
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(
+                      top: 45,
+                    ),
+                    child: Text(
+                      "Comment Added",
+                      style: Styles.boldH2(AppTheme.dark),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(
+                      top: 20,
+                      left: 25,
+                      right: 25,
+                    ),
+                    child: Text(
+                      "Your comment was succesfully published to this post.",
+                      textAlign: TextAlign.center,
+                      style: Styles.regularLabel(AppTheme.dark80),
+                    ),
+                  ),
+                  Expanded(
+                    child: Center(
+                      child: GestureDetector(
+                        onTap: () {
+                          Navigator.of(context)
+                              .popUntil((route) => route.isFirst);
+                        },
+                        child: Container(
+                          height: 56,
+                          width: double.infinity,
+                          margin: EdgeInsets.only(
+                            left: 25,
+                            right: 25,
+                          ),
+                          decoration: BoxDecoration(
+                            color: AppTheme.primary,
+                            borderRadius: BorderRadius.circular(28.5),
+                            boxShadow: [
+                              BoxShadow(
+                                offset: Offset(0, 6),
+                                blurRadius: 75,
+                                spreadRadius: 0,
+                                color: Color.fromRGBO(100, 87, 87, 0.05),
+                              )
+                            ],
+                          ),
+                          child: Center(
+                            child: Text(
+                              "Done",
+                              style: Styles.boldButton(AppTheme.white),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             );
           },
